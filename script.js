cf38f30fe446abb6b5b030589eb17c8d562929f5
Aqeel <106826671+aqeeluser404@users.noreply.github.com>
2023-10-04 09:48:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// javscript to handle the click 
+const hamburger = document.querySelector(".hamburger");
+const contentWrapper = document.querySelector(".content-wrapper");
+
+hamburger.onclick = function() {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+  contentWrapper.classList.toggle('active');
+}
