40aa62ce10aa6d3a4588a4ae6b8298d937a8f024
aqeeluser404 <aqeelhanslo@gmail.com>
2023-10-04 16:01:57 +0200

    #3 commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// javscript to handle the click 
 const hamburger = document.querySelector(".hamburger");
 const contentWrapper = document.querySelector(".content-wrapper");
 
@@ -7,3 +6,39 @@ hamburger.onclick = function() {
   navBar.classList.toggle("active");
   contentWrapper.classList.toggle('active');
 }
+
+// function typeWriter() {
+//   const text = "Hi, I'm Aqeel, \nSoftware Developer";
+//   let i = 0;
+//   let isReversing = false;
+
+//   function reverseText() {
+//     isReversing = true;
+//     let reversedText = text.split('').reverse().join('');
+//     document.getElementById('typedText').innerHTML = reversedText;
+//   }
+
+//   function type() {
+//     if (i < text.length && !isReversing) {
+//       document.getElementById('typedText').innerHTML += text.charAt(i);
+//       i++;
+//       setTimeout(type, 100);
+//     } else if (i === text.length && !isReversing) {
+//       setTimeout(reverseText, 1000); // Delay before reversing
+//     } else if (isReversing) {
+//       document.getElementById('typedText').innerHTML = text.substr(0, i);
+//       i--;
+//       if (i >= 0) {
+//         setTimeout(type, 100); 
+//       } else {
+//         isReversing = false; 
+//         i = 0;
+//         setTimeout(type, 1000); 
+//       }
+//     }
+//   }
+
+//   type();
+// }
+
+// typeWriter();
