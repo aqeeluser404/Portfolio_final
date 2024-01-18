25932e4beb3013c5dfc5655f83e0e9aa72ed6ba9
aqeeluser404 <aqeelhanslo@gmail.com>
2024-01-18 09:42:32 +0100

    navbar fixes

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
-
+// navbar visibility while scrolling the page
 document.addEventListener("DOMContentLoaded", function() {
   const header = document.querySelector("header");
 
   window.addEventListener("scroll", function() {
       if (window.scrollY > 50) {
-          header.classList.add("header-scroll");
+        header.classList.add("header-scroll");
       } else {
-          header.classList.remove("header-scroll");
+        header.classList.remove("header-scroll");
       }
   });
 });
+
+
+
 // Function for navigation hamburger
 const hamburger = document.querySelector(".hamburger");
-const contentWrapper = document.querySelector(".content-wrapper");
 
 hamburger.onclick = function() {
   navBar = document.querySelector(".navbar");
   navBar.classList.toggle("active");
-  contentWrapper.classList.toggle('active');
+
+  const header = document.querySelector("header");
+    header.classList.add("black-background");
+    
+    if (!navBar.classList.contains("active")) {
+      header.classList.remove("black-background");
+    }
+
 }
 
 
+// Function for redirecting to contact page
+function redirectToPage() {
+  window.location.href = 'https://formsubmit.co/el/jufidu';
+  // <a href="https://formsubmit.co/el/jufidu" target="_blank">Email us</a>
+}
+
+
+
 // Function for card skill containers
-document.querySelectorAll('.skill-card').forEach(card => {
-  let skillBarFilled = card.querySelector('.skill-bar-filled');
-  let timeout;
-
-  card.addEventListener('mouseover', function() {
-      let skill = this.getAttribute('data-skill');
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-          skillBarFilled.style.width = skill + '%';
-      }, 150); 
-  });
-
-  card.addEventListener('mouseout', function() {
-
-      skillBarFilled.style.width = '0%';
-      clearTimeout(timeout);
-  });
-});
+// document.querySelectorAll('.skill-card').forEach(card => {
+//   let skillBarFilled = card.querySelector('.skill-bar-filled');
+//   let timeout;
+//   card.addEventListener('mouseover', function() {
+//       let skill = this.getAttribute('data-skill');
+//       clearTimeout(timeout);
+//       timeout = setTimeout(() => {
+//           skillBarFilled.style.width = skill + '%';
+//       }, 150);
+//   });
+//   card.addEventListener('mouseout', function() {
+//       skillBarFilled.style.width = '0%';
+//       clearTimeout(timeout);
+//   });
+// });
 
 
 
@@ -79,42 +93,4 @@ window.addEventListener('scroll', handleIconVisibility);
 window.addEventListener('scroll', updateMirroredTimeline);
 
 
-function redirectToPage() {
-  // Redirecting to another HTML page
-  window.location.href = 'https://formsubmit.co/el/jufidu';
-  // <a href="https://formsubmit.co/el/jufidu" target="_blank">Email us</a>
 
-}
-// function typeWriter() {
-//   const text = "Hi, I'm Aqeel, \nSoftware Developer";
-//   let i = 0;
-//   let isReversing = false;
-
-//   function reverseText() {
-//     isReversing = true;
-//     let reversedText = text.split('').reverse().join('');
-//     document.getElementById('typedText').innerHTML = reversedText;
-//   }
-
-//   function type() {
-//     if (i < text.length && !isReversing) {
-//       document.getElementById('typedText').innerHTML += text.charAt(i);
-//       i++;
-//       setTimeout(type, 100);
-//     } else if (i === text.length && !isReversing) {
-//       setTimeout(reverseText, 1000); // Delay before reversing
-//     } else if (isReversing) {
-//       document.getElementById('typedText').innerHTML = text.substr(0, i);
-//       i--;
-//       if (i >= 0) {
-//         setTimeout(type, 100); 
-//       } else {
-//         isReversing = false; 
-//         i = 0;
-//         setTimeout(type, 1000); 
-//       }
-//     }
-//   }
-//   type();
-// }
-// typeWriter();
